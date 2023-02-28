0e32a790fce88ec8267caf7471539eb5ce2fcced
kyieon <dldudwo87@gmail.com>
2023-02-28 18:19:03 +0900

    update library  - SSHSyncPoolableObjectFactory (destory, validate, passivate )

--- src/main/java/com/j2s/secure/ssh/SSHSyncPoolableObjectFactory.java
+++ src/main/java/com/j2s/secure/ssh/SSHSyncPoolableObjectFactory.java
@@ -1,6 +1,7 @@
 package com.j2s.secure.ssh;
 
 import com.j2s.secure.SSHSessionConfig;
+import com.j2s.secure.ssh.ex.SSHSessionException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.pool2.BasePooledObjectFactory;
 import org.apache.commons.pool2.PooledObject;
@@ -26,7 +27,7 @@ public class SSHSyncPoolableObjectFactory extends BasePooledObjectFactory<SSHSyn
 		String sessionKey = UUID.randomUUID().toString();
 		SSHSyncSession session = new SSHSyncSessionImpl(sessionKey);
 		session.connect(sshSessionConfig.getHost(), sshSessionConfig.getPort(), sshSessionConfig.getId(), sshSessionConfig.getPwd());
-		log.info("[" + session.getSessionKey() + "][" + session.toString() + "] create.");
+		log.info("[" + session.getSessionKey() + "][" + session.toString() + "] makeObject.");
 		return session;
 	}
 
@@ -36,68 +37,41 @@ public class SSHSyncPoolableObjectFactory extends BasePooledObjectFactory<SSHSyn
 	}
 
 	@Override
-	public PooledObject<SSHSyncSession> makeObject() throws Exception {
-		return super.makeObject();
+	public void destroyObject(PooledObject<SSHSyncSession> p) throws Exception {
+		SSHSyncSession session = p.getObject();
+		synchronized (session) {
+			log.info("[" + session.getSessionKey() + "][" + session.toString() + "] destroyObject.");
+			session.close();
+		}
 	}
 
-	//
-//	@Override
-//	public Object makeObject() throws Exception {
-//		String sessionKey = UUID.randomUUID().toString();
-//		SSHSyncSession session = new SSHSyncSessionImpl(sessionKey);
-//		session.connect(sshSessionConfig.getHost(), sshSessionConfig.getPort(), sshSessionConfig.getId(), sshSessionConfig.getPwd());
-//		log.info("[" + session.getSessionKey() + "][" + session.toString() + "] makeObject.");
-//		return session;
-//	}
-//
-//	@Override
-//	public void destroyObject(Object obj) throws Exception {
-//		SSHSyncSession session = convertSessionObj(obj);
-//		synchronized (session) {
-//			log.info("[" + session.getSessionKey() + "][" + session.toString() + "] destroyObject.");
-//			session.close();
-//		}
-//	}
-//
-//	@Override
-//	public boolean validateObject(Object obj) {
-//		SSHSyncSession session = null;
-//		try {
-//			session = convertSessionObj(obj);
-//			synchronized (session) {
-//				log.info("[" + session.getSessionKey() + "][" + session.toString() + "] validateObject.");
-//				if(!session.isConnected())
-//					throw new SSHSessionException("session is not connected.");
-//				session.write("pwd", 3);
-//				return true;
-//			}
-//		} catch (Exception e) {
-//			String errorMsg = "";
-//			if(null != session) {
-//				errorMsg = "[" + session.getSessionKey() + "] validateObject fail.";
-//			}
-//			log.error(errorMsg, e);
-//		}
-//		return false;
-//	}
-//
-//	@Override
-//	public void passivateObject(Object obj) throws Exception {
-//		SSHSyncSession session = convertSessionObj(obj);
-//		synchronized (session) {
-//			log.info("[" + session.getSessionKey() + "][" + session.toString() + "] passivateObject.");
-//			try {
-//				session.write("cd ~", 3);
-//			} catch (Exception e) {
-//				throw new IllegalStateException(e); // Pool closed
-//			}
-//		}
-//	}
-//
-//	protected SSHSyncSession convertSessionObj(Object obj) throws SSHSessionException {
-//		if(obj instanceof SSHSyncSession) {
-//			return (SSHSyncSession) obj;
-//		}
-//		throw new SSHSessionException("class cast error. " + obj.getClass().getName());
-//	}
+	@Override
+	public boolean validateObject(PooledObject<SSHSyncSession> p) {
+		SSHSyncSession session = p.getObject();
+		try {
+			synchronized (session) {
+				log.info("[" + session.getSessionKey() + "][" + session.toString() + "] validateObject.");
+				if(!session.isConnected())
+					throw new SSHSessionException("session is not connected.");
+				session.write("pwd", 3);
+				return true;
+			}
+		} catch (Exception e) {
+			log.error("[" + session.getSessionKey() + "] validateObject fail.", e);
+		}
+		return false;
+	}
+
+	@Override
+	public void passivateObject(PooledObject<SSHSyncSession> p) throws Exception {
+		SSHSyncSession session = p.getObject();
+		synchronized (session) {
+			log.info("[" + session.getSessionKey() + "][" + session.toString() + "] passivateObject.");
+			try {
+				session.write("cd ~", 3);
+			} catch (Exception e) {
+				throw e; // Pool closed
+			}
+		}
+	}
 }
